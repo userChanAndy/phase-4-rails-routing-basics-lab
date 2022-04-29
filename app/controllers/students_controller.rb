d17f18f92be1247a0c552c0b2718ed053bff713f
Andy Chan <andy.chan9930@gmail.com>
2022-04-29 16:50:35 -0400

    commit tree cdbc995dd8bf013e1c689a301312b06919b7e460

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,14 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+    def grades
+        grades = Student.order(grade: :desc)
+        render json: grades
+    end
+    def highest_grade
+        highest_grade = Student.order(grade: :desc).first
+        render json: highest_grade
+    end
+end
